557a19988601abb3366292f53d1b48e14c9ead46
aluz <aluzdev@gmail.com>
2024-02-04 17:13:02 -0600

    fix: server going down randomly

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,21 +1,19 @@
 const Post = require("../models/posts");
 
 module.exports.getPosts = async (req, res) => {
-  const posts = await postModel.find();
+  const posts = await Post.find();
   res.send(posts);
 };
 
 module.exports.createPost = async (req, res) => {
-
   try {
- 
     const data = await Post.create(req.body);
-    await data.save()
-    
+    await data.save();
+
     console.log(`post saved successfuly:`, data);
     res.status(201).send(data);
 
-    console.log(data)
+    console.log(data);
   } catch (err) {
     console.error(err);
     res.status(400).send({ error: err, msg: "Could not create post!" });
