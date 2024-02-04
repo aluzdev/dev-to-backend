036d6dd8b9bc6bd530e44b929bb0c8c9e5742175
aluzdev <aluzdev@gmail.com>
2024-02-04 13:16:56 -0700

    post works

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,3 +4,17 @@ module.exports.getPosts = async (req, res) => {
   const posts = await postModel.find();
   res.send(posts);
 };
+
+module.exports.createPost = async (req, res) => {
+  try {
+    const { post } = req.body;
+    const data = await postModel.create({ post });
+    console.log(`post saved successfuly:`, data);
+    res.status(201).send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({ error: err, msg: "Could not create post!" });
+  }
+};
+
+//TODO: add update and delete controllers
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { getPosts } = require("../controllers/posts");
+const { getPosts, createPost } = require("../controllers/posts");
 
 const router = Router(); // Use express.Router() to create a router instance
 
 router.get("/get", getPosts);
+router.post("/create", createPost);
+//TODO: add .put and .delete
 
 module.exports = router;
