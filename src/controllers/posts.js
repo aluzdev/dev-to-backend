279677eb56480c311f8ecd3ac8ae848f06928149
makreno <makreno@gmail.com>
2024-02-05 18:29:43 -0600

    se agrega getPostById

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,5 +1,5 @@
 const Post = require("../models/posts");
-
+/*
 module.exports.getPosts = async (req, res) => {
   const posts = await Post.find();
   res.send(posts);
@@ -20,4 +20,46 @@ module.exports.createPost = async (req, res) => {
   }
 };
 
+module.exports.getPostById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    res.send({ msg: "user", data: post });
+  } catch (error) {
+    res.status(400).send({ msg: "can not get user", error: error });
+  }
+};
+*/
+
+module.exports = {
+  getPosts: async (req, res) => {
+    const posts = await Post.find();
+    res.send(posts);
+  },
+
+  createPost: async (req, res) => {
+    try {
+      const data = await Post.create(req.body);
+      await data.save();
+
+      console.log(`post saved successfuly:`, data);
+      res.status(201).send(data);
+
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({ error: err, msg: "Could not create post!" });
+    }
+  },
+
+  getPostById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const post = await Post.findById(id);
+      res.send({ msg: "user", data: post });
+    } catch (error) {
+      res.status(400).send({ msg: "can not get user", error: error });
+    }
+  },
+};
 //TODO: add update and delete controllers
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,22 @@
+/*
 const { Router } = require("express");
-const { getPosts, createPost } = require("../controllers/posts");
+const { getPosts, createPost, getPostById } = require("../controllers/posts");
 
 const router = Router(); // Use express.Router() to create a router instance
 
 router.get("/get", getPosts);
 router.post("/create", createPost);
+router.get("/get/:id"), getPostById;
 //TODO: add .put and .delete
+*/
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/posts");
+
+const PostController = require("../controllers/posts");
+
+router.get("/get", PostController.getPosts);
+router.get("/create", PostController.createPost);
+router.get("/get/:id", PostController.getPostById);
 
 module.exports = router;
