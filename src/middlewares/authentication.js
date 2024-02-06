df4da84386936a4f7b6ea68420be6a088fbaa9d5
aluzdev <aluzdev@gmail.com>
2024-02-06 14:46:01 -0700

    login tested

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,29 +1,31 @@
 const jwt = require("jsonwebtoken");
 const JWT_SIGN = process.env.JWT_SIGN;
+const User = require("../models/users");
 
 async function createJWT(data) {
   return jwt.sign(data, JWT_SIGN, { expiresIn: "7d" });
 }
 
 function verifyJWT(req, res, next) {
-  try {
-    const token = req.headers["bearerauth"];
-    const now = new Date();
-    if (!token) {
-      res.status(401).send({ msg: "login required" });
-    }
-    jwt.verify(token, JWT_SIGN, (err, decode) => {
-      if (err) res.status(401).send({ msg: "invalid token" });
-      const tokenExpired = decode.exp < now.getDate() / 1000;
-      if (tokenExpired) {
-        res.status(401).send({ msg: "session expired" });
-      }
-      req.user = decode;
-      next();
-    });
-  } catch (err) {
-    res.status(401).send({ msg: "invalid token", error: err });
+  let token = req.headers.authorization;
+  const dateNow = new Date();
+
+  if (!token) {
+    res.status(401).send({ msg: "login is required" });
   }
+
+  token = token.split(" ")[1];
+  jwt.verify(token, JWT_SIGN, async (err, decode) => {
+    if (err) {
+      res.status(401).send({ msg: "token invalid" });
+    }
+    if (decode.exp < dateNow.getDate() / 1000) {
+      res.status(401).send({ msg: "session expired" });
+    } else {
+      req.user = await User.findById(decode._id);
+      next();
+    }
+  });
 }
 
 module.exports = {
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
 const { userLogin } = require("../controllers/login");
+const { verifyJWT } = require("../middlewares/authentication");
+
+router.post("/login", userLogin);
+
+router.use(verifyJWT); // ESTE MIDDLEWARE ES VALIDO PARA TODOS LOS ENDPOINTS QUE ESTAN DEBAJO
 
 const { getUsers, createUser, getUserById } = require("../controllers/users");
 
 router.get("/get", getUsers);
 router.post("/create", createUser);
 router.get("/get/:id", getUserById);
-
-router.post("/login", userLogin);
 
 module.exports = router;
