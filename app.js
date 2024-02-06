4bca0584fe1fda551752f7471d0891e8afb8270e
Francisco Sanchez <109924284+MXCAZ@users.noreply.github.com>
2024-02-05 21:16:29 -0700

    arreglando conflictos (#6)

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const postRoutes = require("./src/routes/posts");
+const userRoutes = require("./src/routes/users");
 
 const app = express();
-const port = process.env.PORT | 5000;
+const port = process.env.PORT | 5001;
 
 app.use(express.json());
 
@@ -14,5 +15,6 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
