a00eafe4d4463e8e6f8b900d860eb193ff8c7c1a
makreno <makreno@gmail.com>
2024-02-05 20:38:04 -0600

    fix the comments and cancer issues

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,35 +1,4 @@
 const Post = require("../models/posts");
-/*
-module.exports.getPosts = async (req, res) => {
-  const posts = await Post.find();
-  res.send(posts);
-};
-
-module.exports.createPost = async (req, res) => {
-  try {
-    const data = await Post.create(req.body);
-    await data.save();
-
-    console.log(`post saved successfuly:`, data);
-    res.status(201).send(data);
-
-    console.log(data);
-  } catch (err) {
-    console.error(err);
-    res.status(400).send({ error: err, msg: "Could not create post!" });
-  }
-};
-
-module.exports.getPostById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const post = await Post.findById(id);
-    res.send({ msg: "user", data: post });
-  } catch (error) {
-    res.status(400).send({ msg: "can not get user", error: error });
-  }
-};
-*/
 
 module.exports = {
   getPosts: async (req, res) => {
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,22 +1,12 @@
-/*
-const { Router } = require("express");
-const { getPosts, createPost, getPostById } = require("../controllers/posts");
-
-const router = Router(); // Use express.Router() to create a router instance
-
-router.get("/get", getPosts);
-router.post("/create", createPost);
-router.get("/get/:id"), getPostById;
-//TODO: add .put and .delete
-*/
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/posts");
 
-const PostController = require("../controllers/posts");
+const { getPosts, createPost, getPostById } = require("../controllers/posts");
 
-router.get("/get", PostController.getPosts);
-router.get("/create", PostController.createPost);
-router.get("/get/:id", PostController.getPostById);
+router.get("/get", getPosts);
+router.post("/create", createPost);
+router.get("/get/:id", getPostById);
+//TODO: add .put and .delete
 
 module.exports = router;
