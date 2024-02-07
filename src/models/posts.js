39f9f6525a25c45a1aa5d02928e8f940fc75f969
Fany Alvarez <85903638+fanyalvarez@users.noreply.github.com>
2024-02-07 11:39:28 -0600

    add put method of posts and some changes in model (#9)

--- src/models/posts.js
+++ src/models/posts.js
@@ -9,28 +9,41 @@ const mongoose = require("mongoose");
 // };
 
 const postSchema = new mongoose.Schema({
-  
-    author:
-    {
-      type: String,
-      require: true
-    },
-    title: String,
-    content: String,
-    creationDate: Date,
-    rating: Number,
-    relevant: Boolean,
-    tags: String,
-    avatar: {
-      type: String,
-      // validate: urlValidator,
-    },
-    image: {
-      type: String,
-      // validate: urlValidator,
-    },
-  
-});
+  author: {
+    type: String,
+    require: true
+    // type: mongoose.Schema.Types.ObjectId,
+    // ref: "user",
+  },
+  avatar: {
+    type: String,
+    // validate: urlValidator,
+  },
+  title: {
+    type: String,
+    require: true
+  },
+  content: {
+    type: String,
+    require: true
+  },
+  // creationDate: Date,
+  rating: {
+    type: String,
+  },
+  relevant: {
+    type: Boolean,
+  },
+  tags: {
+    type: String,
+  },
+  image: {
+    type: String,
+    // validate: urlValidator,
+  }},
+
+  { timestamps: { createdAt: true, updatedAt: true } }
+);
 
 // module.exports = mongoose.model("Post", postSchema);
 const Post = mongoose.model("posts", postSchema)
