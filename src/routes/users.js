aae5e228eebfad097eb1312e8ec667469c292254
aluzdev <aluzdev@gmail.com>
2024-02-07 18:34:19 -0700

    removed unused import

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
-const { verifyJWT } = require("../middlewares/authentication");
 const {
   getUsers,
   createUser,
