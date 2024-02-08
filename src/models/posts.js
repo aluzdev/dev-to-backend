84b004bec5be3022bd93b15351291ab1aa3bcc5a
MXCAZ <final00717@hotmail.com>
2024-02-07 21:15:19 -0700

    .

--- src/models/posts.js
+++ src/models/posts.js
@@ -8,7 +8,6 @@ const mongoose = require("mongoose");
 //   message: "Invalid URL format",
 // };
 
-<<<<<<< HEAD
 const postSchema = new mongoose.Schema(
   {
     author: {
@@ -47,28 +46,6 @@ const postSchema = new mongoose.Schema(
 
   { timestamps: { createdAt: true, updatedAt: true } }
 );
-=======
-const postSchema = new mongoose.Schema({
-  author: {
-    type: String,
-    require: true,
-  },
-  title: String,
-  content: String,
-  creationDate: Date,
-  rating: Number,
-  relevant: Boolean,
-  tags: String,
-  avatar: {
-    type: String,
-    // validate: urlValidator,
-  },
-  image: {
-    type: String,
-    // validate: urlValidator,
-  },
-});
->>>>>>> 568220d40a7d707c7d8cce0f8d261aec7f656d14
 
 const Post = mongoose.model("posts", postSchema);
 
--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
@@ -31,26 +30,6 @@ const userSchema = new mongoose.Schema(
     },
   }
 );
-=======
-const { postSchema } = require("./posts");
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [
-      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-      "Ingresa un correo valido: ejemplo juan.perez@gmail.com",
-    ],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  savedPost: [postSchema], // Usar el esquema de posts para definir los posts guardados
-});
->>>>>>> 568220d40a7d707c7d8cce0f8d261aec7f656d14
 
 const User = mongoose.model("users", userSchema);
 
