b9398a194190406ed145ee87e23c8798358a68c7
aluzdev <aluzdev@gmail.com>
2024-03-03 03:04:13 -0700

    re-restructured backend to charles style

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const server = require("./src/server");
+const db = require("./src/lib/db");
+
+const port = process.env.PORT || 3000;
+
+db.connect()
+  .then(() => {
+    console.log("DB connected");
+
+    server.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("DB Error: ", error);
+  });
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
